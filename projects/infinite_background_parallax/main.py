e216dd76e1b47a8da5daa2a9425bbf8e5ee97806
Jonathan Bouchet <bouchetjonathan@gmail.com>
2026-06-15 17:13:45 -0700

    big refactoring into Textures classes ; much straightforward code

--- projects/infinite_background_parallax/main.py
+++ projects/infinite_background_parallax/main.py
@@ -1,6 +1,60 @@
 from pathlib import Path
 import pyray as pr
 
+"""
+refactoring into a class
+Class Texture:
+    # does not need moving or update; fixed texture
+    draw
+
+Class Texture2(Texture)
+    # derive from Texture ; need update and moving texture
+    update
+    move
+
+Class BackGround()
+    # class to hold all background textures of the game
+    textures: list[Texture]    
+"""
+
+
+class BaseTexture:
+    def __init__(self, position: pr.Vector2, texture_path: str) -> None:
+        self.position = position
+        self.texture = pr.load_texture(texture_path)
+
+    def draw(self) -> None:
+        pr.draw_texture_v(self.texture, self.position, pr.WHITE)
+
+    def update(self, dt: float):
+        pass
+
+
+class Texture(BaseTexture):
+    def __init__(
+        self, position: pr.Vector2, texture_path: str, speed: float, offset: int
+    ) -> None:
+        super().__init__(position=position, texture_path=texture_path)
+        self.speed = speed
+        self.offset = offset
+
+    def update(self, dt: float):
+        self.position.x -= dt * self.speed
+        if (self.position.x + self.texture.width) < 0:
+            self.position.x = self.offset * self.texture.width
+
+
+class Background:
+    def __init__(self, textures: list[Texture]) -> None:
+        self.textures = textures
+
+    def update(self, dt) -> None:
+        _ = [texture.update(dt) for texture in self.textures]
+
+    def draw(self, dt) -> None:
+        _ = [texture.draw() for texture in self.textures]
+
+
 width, height = 700, 432
 
 THIS_DIR = (Path(__file__).parent / "assets").resolve()
@@ -8,78 +62,75 @@ THIS_DIR = (Path(__file__).parent / "assets").resolve()
 pr.init_window(width, height, "app")
 pr.set_target_fps(60)
 
-fixed_background = pr.load_texture(f"{THIS_DIR}/plx-1.png")
-fixed_background_pos = pr.Vector2()
+fixed_background = BaseTexture(
+    position=pr.Vector2(), texture_path=f"{THIS_DIR}/plx-1.png"
+)
+foreground_1 = Texture(
+    position=pr.Vector2(0, height - 50),
+    texture_path=f"{THIS_DIR}/ground.png",
+    speed=100,
+    offset=3,
+)
+foreground_2 = Texture(
+    position=pr.Vector2(foreground_1.texture.width, height - 50),
+    texture_path=f"{THIS_DIR}/ground.png",
+    speed=100,
+    offset=3,
+)
+foreground_3 = Texture(
+    position=pr.Vector2(2 * foreground_2.texture.width, height - 50),
+    texture_path=f"{THIS_DIR}/ground.png",
+    speed=100,
+    offset=3,
+)
+foreground_4 = Texture(
+    position=pr.Vector2(3 * foreground_3.texture.width, height - 50),
+    texture_path=f"{THIS_DIR}/ground.png",
+    speed=100,
+    offset=3,
+)
 
-foreground_1 = pr.load_texture(f"{THIS_DIR}/ground.png")
-foreground_2 = foreground_1
-foreground_3 = foreground_1
-foreground_4 = foreground_1
-foreground_pos_1 = pr.Vector2(0, height - 50)
-foreground_pos_2 = pr.Vector2(foreground_1.width, height - 50)
-foreground_pos_3 = pr.Vector2(2 * foreground_2.width, height - 50)
-foreground_pos_4 = pr.Vector2(3 * foreground_3.width, height - 50)
+background_1 = Texture(
+    position=pr.Vector2(0, 0), texture_path=f"{THIS_DIR}/plx-5.png", speed=50, offset=1
+)
+background_2 = Texture(
+    position=pr.Vector2(background_1.texture.width, 0),
+    texture_path=f"{THIS_DIR}/plx-5.png",
+    speed=50,
+    offset=1,
+)
 
-background_11 = pr.load_texture(f"{THIS_DIR}/plx-5.png")
-background_12 = background_11
-background_pos_11 = pr.Vector2(0, 0)
-background_pos_12 = pr.Vector2(background_11.width, 0)
+background_3 = Texture(
+    position=pr.Vector2(0, 0), texture_path=f"{THIS_DIR}/plx-4.png", speed=20, offset=1
+)
+background_4 = Texture(
+    position=pr.Vector2(background_3.texture.width, 0),
+    texture_path=f"{THIS_DIR}/plx-4.png",
+    speed=50,
+    offset=1,
+)
 
-background_21 = pr.load_texture(f"{THIS_DIR}/plx-4.png")
-background_22 = background_21
-background_pos_21 = pr.Vector2(0, 0)
-background_pos_22 = pr.Vector2(background_21.width, 0)
-
+# order matters !
+background = Background(
+    textures=[
+        fixed_background,
+        background_3,
+        background_4,
+        background_1,
+        background_2,
+        foreground_1,
+        foreground_2,
+        foreground_3,
+        foreground_4,
+    ]
+)
 
 while not pr.window_should_close():
     dt = pr.get_frame_time()
+    background.update(dt=dt)
     pr.begin_drawing()
     pr.clear_background(pr.BLACK)
-
-    foreground_pos_1.x -= dt * 100
-    foreground_pos_2.x -= dt * 100
-    foreground_pos_3.x -= dt * 100
-    foreground_pos_4.x -= dt * 100
-
-    background_pos_11.x -= dt * 50
-    background_pos_12.x -= dt * 50
-
-    background_pos_21.x -= dt * 20
-    background_pos_22.x -= dt * 20
-
-    # draw fix background first
-    pr.draw_texture_v(fixed_background, fixed_background_pos, pr.WHITE)
-
-    pr.draw_texture_v(background_21, background_pos_21, pr.WHITE)
-    pr.draw_texture_v(background_22, background_pos_22, pr.WHITE)
-
-    pr.draw_texture_v(background_11, background_pos_11, pr.WHITE)
-    pr.draw_texture_v(background_12, background_pos_12, pr.WHITE)
-
-    pr.draw_texture_v(foreground_1, foreground_pos_1, pr.WHITE)
-    pr.draw_texture_v(foreground_2, foreground_pos_2, pr.WHITE)
-    pr.draw_texture_v(foreground_3, foreground_pos_3, pr.WHITE)
-    pr.draw_texture_v(foreground_4, foreground_pos_4, pr.WHITE)
-
-    if (foreground_pos_1.x + foreground_1.width) < 0:
-        foreground_pos_1.x = 3 * foreground_1.width
-    if (foreground_pos_2.x + foreground_2.width) < 0:
-        foreground_pos_2.x = 3 * foreground_2.width
-    if (foreground_pos_3.x + foreground_3.width) < 0:
-        foreground_pos_3.x = 3 * foreground_3.width
-    if (foreground_pos_4.x + foreground_4.width) < 0:
-        foreground_pos_4.x = 3 * foreground_4.width
-
-    if (background_pos_21.x + background_21.width) < 0:
-        background_pos_21.x = background_21.width
-    if (background_pos_22.x + background_22.width) < 0:
-        background_pos_22.x = background_22.width
-
-    if (background_pos_11.x + background_11.width) < 0:
-        background_pos_11.x = background_11.width
-    if (background_pos_12.x + background_12.width) < 0:
-        background_pos_12.x = background_12.width
-
+    background.draw(dt)
     pr.end_drawing()
 
 pr.close_window()
