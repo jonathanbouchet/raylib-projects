fcb4687b39865eac90b90875116484c0d934cf82
Jonathan Bouchet <bouchetjonathan@gmail.com>
2026-07-15 17:19:29 -0700

    added async loop, added grid

--- src/template.py
+++ src/template.py
@@ -1,16 +1,29 @@
+import asyncio
 import pyray as pr
 
 width, height = 600, 600
 
-pr.init_window(width, height, "app")
-pr.set_target_fps(60)
 
-while not pr.window_should_close():
-    # logic here
-    # rendering
-    pr.begin_drawing()
-    pr.clear_background(pr.BLACK)
-    pr.draw_fps(0,0)
-    pr.end_drawing()
+async def main() -> None:
 
-pr.close_window()
+    pr.init_window(width, height, "app")
+    pr.set_target_fps(60)
+
+    while not pr.window_should_close():
+        # logic here
+
+        # rendering
+        pr.begin_drawing()
+        pr.clear_background(pr.BLACK)
+        pr.draw_fps(0, 0)
+
+        pr.draw_line(width // 2, 0, width // 2, height, pr.RED)
+        pr.draw_line(0, height // 2, width, height // 2, pr.RED)
+        pr.end_drawing()
+        await asyncio.sleep(0)
+
+    pr.close_window()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
