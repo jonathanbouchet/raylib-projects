ab6b3a77f7717ad855f10636f27dcc0599bd475d
Jonathan Bouchet <bouchetjonathan@gmail.com>
2026-07-21 21:06:53 -0700

    initial commit

---
+++ projects/city_builder/main.py
@@ -0,0 +1,16 @@
+import asyncio
+import pyray as pr
+from src.game import Game
+
+
+async def main() -> None:
+    game = Game(
+        width=640, height=480, fps_target=60, name="app", background_color=pr.BLACK
+    )
+    game.init()
+    await game.run()
+    game.end()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
