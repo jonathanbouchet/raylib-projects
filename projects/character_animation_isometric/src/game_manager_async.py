8cf756e1f0ad501d36a2ba3580546c0a1dbf32e9
Jonathan Bouchet <bouchetjonathan@gmail.com>
2026-08-07 13:41:25 -0700

    added character with loaded animations ; added bsic UI dropdown to select animation

--- projects/character_animation_isometric/src/game_manager_async.py
+++ projects/character_animation_isometric/src/game_manager_async.py
@@ -1,5 +1,8 @@
 import asyncio
 import pyray as pr
+import raylib as rl
+from .utils import load_textures
+from src.character import Character, States
 
 
 class Game:
@@ -16,13 +19,30 @@ class Game:
         self.fps_target = fps_target
         self.name = name
         self.background_color = background_color
+        self.textures = {}
+        self.active_index_ptr:int = pr.ffi.new("int *", 0)
+        self.ui_dropdown_edit_mode = False
+        self.ui_selected_value: int = None
 
     def init(self):
         pr.init_window(self.width, self.height, self.name)
         pr.set_target_fps(self.fps_target)
+        self.textures = load_textures()
+        for k,v in self.textures.items():
+            print(f"animation: {k}: {len(v)} animations")
+        self.character = Character(position=pr.Vector2(0, 0), textures=self.textures)
 
     def update(self) -> None:
-        pass
+        if pr.gui_dropdown_box(
+            pr.Rectangle(0, 40, 120, 20),
+                "IDLE;RUN;PICKUP",
+                self.active_index_ptr,
+                self.ui_dropdown_edit_mode,
+            ):
+                self.ui_dropdown_edit_mode = not self.ui_dropdown_edit_mode
+                self.ui_selected_value = self.active_index_ptr[0]
+
+        self.character.update(selected_value=self.ui_selected_value)
 
     async def run(self) -> None:
         while not pr.window_should_close():
@@ -31,10 +51,13 @@ class Game:
             await asyncio.sleep(0)
 
     def draw(self) -> None:
+        dt = pr.get_frame_time()
         pr.begin_drawing()
         pr.clear_background(self.background_color)
+        self.character.draw(dt=dt)
         pr.end_drawing()
         pr.draw_fps(0,0)
+        pr.draw_text(f"{self.character.state}", 0, 20, 20, pr.GREEN)
 
     def end(self) -> None:
         pr.close_window()
---
+++ projects/character_animation_isometric/src/utils.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import pyray as pr
+
+THIS_DIR = (Path(__file__).parent.parent / "assets/animations").resolve()
+print(f"{str(THIS_DIR)=}")
+
+
+def load_textures() -> dict[str, list[pr.Texture]]:
+    """load_textures for different animations
+    each animation contains severals png
+
+    :return: _description_
+    :rtype: dict[str, list[pr.Texture2D]]
+    """
+    textures = {}
+    for anim in ["idle_SE", "run_SE", "pickup_SE"]:
+        name_anim: str = ""
+        if anim == "idle_SE":
+            name_anim = "Idle"
+        elif anim == "run_SE":
+            name_anim = "Run"
+        elif anim == "pickup_SE":
+            name_anim = "Pickup"
+        else:
+            print(f"texture not found for :{anim}")
+            return
+        anim_dir = Path(f"{THIS_DIR}/{anim}")
+        file_count = sum(1 for x in anim_dir.iterdir() if x.is_file())
+        textures[anim] = [
+            pr.load_texture(f"{str(anim_dir)}/Male_2_{name_anim}{i}.png") for i in range(file_count)
+        ]
+    return textures
