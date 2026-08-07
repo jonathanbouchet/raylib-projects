863d5d7d73e43452e94967049749b03459c23dda
Jonathan Bouchet <bouchetjonathan@gmail.com>
2026-08-07 09:55:54 -0700

    initial commit

---
+++ projects/character_animation_isometric/README.md
@@ -0,0 +1,4 @@
+## goal
+
+- This repo is for displaying an isometric character's animation
+- serve as the code-base for other of personal projects
---
+++ projects/character_animation_isometric/main.py
@@ -0,0 +1,15 @@
+import asyncio
+import pyray as pr
+from src.game_manager_async import Game
+
+async def main() -> None:
+    game = Game(
+        width=600, height=600, fps_target=60, name="app", background_color=pr.BLACK
+    )
+    game.init()
+    await game.run()
+    game.end()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
